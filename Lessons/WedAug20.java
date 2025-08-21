cc292ed4cf3c348da5847d39fac0509166d3ac70
laclustr <28vance.grant@rrcs.org>
2025-08-20 21:15:15 -0400

    added new lesson and boredom file, fixed boredom folder creater slightly

---
+++ Lessons/WedAug20.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class WedAug20 {
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		String line = scann.nextLine();
+		System.out.println("You said: " + line);
+	}
+}
