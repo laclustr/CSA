d12966e8375dea01aa8f13552ebacba6913316b5
laclustr <28vance.grant@rrcs.org>
2025-08-22 13:09:58 -0400

    new lesson

---
+++ Lessons/FriAug22.java
@@ -0,0 +1,9 @@
+public class FriAug22 {
+	public static void main(String[] args) {
+		int asquared = 'a' * 'a';
+		int bsquared = 'b' * 'b';
+		int csquared = 'c' * 'c';
+
+		System.out.println((float) (asquared + bsquared) / csquared);
+	}
+}
