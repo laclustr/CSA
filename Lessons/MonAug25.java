a6c3624a7e19670950f2fb6298b90b3f3e16335a
laclustr <28vance.grant@rrcs.org>
2025-08-25 13:11:28 -0400

    new lesson

---
+++ Lessons/MonAug25.java
@@ -0,0 +1,9 @@
+public class MonAug25 {
+	public static void main(String[] args) {
+		System.out.println(inRangeTen(11));
+	}
+
+	public static boolean inRangeTen(int x) {
+		return x <= 10 && x >= 0;
+	}
+}
