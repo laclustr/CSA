641436e421b8ecac72c3595183ebf7bb510bead9
laclustr <28vance.grant@rrcs.org>
2025-10-14 13:12:11 -0400

    new lesson

--- Challenge Problems/BinarySearch/BinarySearch.java
+++ Challenge Problems/BinarySearch/BinarySearch.java
@@ -13,7 +13,78 @@ public class BinarySearch {
 		Arrays.sort(arr);
 
 		// System.out.println(arr2Str(arr));
-		System.out.println(sqrt(9));
+		System.out.println(findMedianSorted(new int[] {1,5,5,6,8}, new int[] {4,6,10}));
+	}
+
+	public static double findMedianSorted(int[] arr1, int[] arr2) {
+		if (arr1.length < arr2.length) {
+			int[] temp = arr1;
+			arr1 = arr2;
+			arr2 = temp;
+		}
+
+		if (arr2.length == 0) {
+			if (arr1.length % 2 == 0) return (arr1[arr1.length / 2] + arr1[arr1.length / 2 + 1]) / 2.d;
+			else return arr1[arr1.length / 2];
+		}
+
+		int left = 0;
+		int right = arr1.length;
+
+		while (left <= right) {
+			int part1 = (left + right) / 2;
+			int part2 = (arr1.length + arr2.length + 1) / 2 - part1;
+
+			int maxL1 = (part1 == 0) ? Integer.MIN_VALUE : arr1[part1 - 1];
+			int minR1 = (part1 == arr1.length) ? Integer.MAX_VALUE : arr1[part1];
+			int maxL2 = (part2 == 0) ? Integer.MIN_VALUE : arr2[part2 - 1];
+			int minR2 = (part2 == arr2.length) ? Integer.MAX_VALUE : arr2[part2];
+
+			if (maxL1 <= minR2 && maxL2 <= minR1) {
+				if ((arr1.length + arr2.length) % 2 == 0) return (Math.max(maxL1, maxL2) + Math.min(minR1, minR2)) / 2.d;
+				else return Math.max(maxL1, maxL2);
+			} else if (maxL1 > minR2) {
+				right = part1 - 1;
+			} else {
+				left = part1 + 1;
+			}
+ 		}
+ 		return 0;
+	}
+
+	public static int findMin(int[] arr) {
+		int left = 0;
+		int right = arr.length - 1;
+
+		while (left < right) {
+			int mid = (left + right) / 2;
+
+			if (arr[mid] > arr[right]) left = mid + 1;
+			else right = mid;
+		}
+
+		return arr[left];
+	}
+
+	public static int findShifted(int[] arr, int key) {
+		int left = 0;
+		int right = arr.length - 1;
+
+		while (left <= right) {
+			int mid = (left + right) / 2;
+
+			if (arr[mid] == key) return mid;
+
+			if (arr[left] <= arr[mid]) {
+				if (key >= arr[left] && key < arr[mid]) right = mid - 1;
+				else left = mid + 1;
+			} else {
+				if (key > arr[mid] && key <= arr[right]) left = mid + 1;
+				else right = mid - 1;
+			}
+		}
+
+		return -1;
 	}
 
 	public static int sqrt(int x) {
@@ -90,5 +161,4 @@ public class BinarySearch {
 
 		return s.toString();
 	}
-
 }
