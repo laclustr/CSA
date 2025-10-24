d01733ef5704605c96e4d1027bf6eb59eb923a6a
laclustr <28vance.grant@rrcs.org>
2025-10-24 13:11:04 -0400

    1 problem left

--- Problem Sets/CSA-Pset3/GrantVancePset3.java
+++ Problem Sets/CSA-Pset3/GrantVancePset3.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class GrantVancePset3 {
 	public static void main(String[] args) {
@@ -38,12 +39,42 @@ public class GrantVancePset3 {
 		shiftElements(arr);
 		System.out.println(Arrays.toString(arr)); // [1, 0, 0, 2, 3, 0, 0, 4]
 
-		// arr = new int[] {2, 0, 2, 1, 1, 0, 1};
-		// sortColors(arr);
-		// System.out.println(Arrays.toString(arr)); // [2, 2, 0, 0, 1, 1, 1]
+		arr = new int[] {2, 0, 2, 1, 1, 0, 1};
+		sortColors(arr);
+		System.out.println(Arrays.toString(arr)); // [2, 2, 0, 0, 1, 1, 1]
 
 		System.out.println(indexOf("hello", "ll")); // 2
 		System.out.println(indexOf("hello", "x")); // -1
+
+		System.out.println(rotatedMin(new int[] {3, 4, 5, 1, 2})); // 1
+		System.out.println(rotatedMin(new int[] {4, 5, 6, 7, 3})); // 3
+
+		System.out.println(minBribes(new int[] {1, 2, 3, 5, 4, 6, 7, 8})); // 1
+		System.out.println(minBribes(new int[] {4, 1, 2, 3})); // -1
+		System.out.println(minBribes(new int[] {2, 1, 5, 3, 4})); // 3
+
+		System.out.println(removePoms(new String[] {"red", "blue", "red", "red", "green"}, new int[] {1, 2, 3, 4, 5})); // 3
+		System.out.println(removePoms(new String[] {"red", "red", "blue", "red", "red"}, new int[] {2, 3, 4, 5, 1})); // 3
+		System.out.println(removePoms(new String[] {"red", "red", "blue", "blue", "green", "red", "red"}, new int[] {1, 2, 3, 4, 5, 6, 7})); // 10
+		
+		System.out.println(fineCalculator(2, 3, "CJ?CC?")); // 5
+		System.out.println(fineCalculator(2, 5, "??J???")); // 0
+		System.out.println(fineCalculator(1, 3, "C?J")); // 1
+
+		prizeMoney(363); // 333 30
+		prizeMoney(6666); // 3333 3333
+		prizeMoney(7636); // 7333 303
+
+		System.out.println(swapsToSave(new String[] {"S", "C", "C", "S", "S", "C"}, 6)); // 2
+		System.out.println(swapsToSave(new String[] {"S", "S"}, 1)); // -1
+		System.out.println(swapsToSave(new String[] {"C", "S", "C", "S", "S"}, 3)); // 5
+
+		try {
+			File f = new File("message.txt");
+			decodeMessage(f);
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		}
 	}
 
 	// Problem 1
@@ -202,7 +233,20 @@ public class GrantVancePset3 {
 	}
 
 	// Problem 12
+	public static void sortColors(int[] arr) {
+		int low = 0;
+		int mid = 0;
+		int high = arr.length - 1;
 
+		while (mid <= high) {
+			if (arr[mid] == 2) {
+				swap(arr, low++, mid++);
+			} else if (arr[mid] == 0) mid++;
+			else {
+				swap(arr, mid, high--);
+			}
+		}
+	}
 
 	// Problem 13
 	public static int indexOf(String str, String key) {
@@ -212,6 +256,153 @@ public class GrantVancePset3 {
 		return -1;
 	}
 
+	// Problem 14
+	public static int rotatedMin(int[] arr) {
+		int left = 0;
+		int right = arr.length - 1;
+
+		while (left < right) {
+			int mid = (left + right) / 2;
+
+			if (arr[mid] > arr[right]) left = mid + 1;
+			else right = mid;
+		}
+		return arr[left];
+	}
+
+	// Problem 15
+	public static int minBribes(int[] arr) {
+		int count = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] - (i + 1) > 2) return -1;
+
+			for (int j = Math.max(0, arr[i] - 2); j < i; j++) {
+				if (arr[j] > arr[i]) count++;
+			}
+		}
+		return count;
+	}
+
+	// Problem 16
+	public static int removePoms(String[] poms, int[] times) {
+		int time = 0;
+
+		for (int i = 0; i < poms.length - 1; i++) {
+			if (poms[i].equals(poms[i + 1])) {
+				if (times[i] < times[i + 1]) time += times[i];
+				else {
+					time += times[i + 1];
+					times[i + 1] = times[i];
+				}
+			}
+		}
+		return time;
+	}
+
+	// Problem 17
+	public static int fineCalculator(int fine1, int fine2, String mural) {
+		char prev = '?';
+		int sum = 0;
+		for (int i = 0; i < mural.length(); i++) {
+			char curr = mural.charAt(i);
+			if (curr == '?') continue;
+
+			if (prev != '?' && prev != curr) {
+				if (prev == 'C' && curr == 'J') sum += fine1;
+				else if (prev == 'J' && curr == 'C') sum += fine2;
+			}
+
+			prev = curr;
+		}
+		return sum;
+	}
+
+	// Problem 18
+	public static void prizeMoney(int N) {
+		int first = 0;
+		int second = 0;
+
+		for (int place = 1; N > 0; place*=10) {
+			int k = N % 10;
+			N /= 10;
+			if (k == 6) {
+				first += place * 3;
+				second += place * 3;
+			}
+			else {
+				first += k * place;
+			}
+		}
+		System.out.println(first + " " + second);
+	}
+	// End Problem 18
+
+	private static int calcDamage(String[] instr) {
+		int power = 1;
+		int damage = 0;
+
+		for (String inst : instr) {
+			if (inst.equals("C")) {
+				power *= 2;
+			} else if (inst.equals("S")) {
+				damage += power;
+			}
+		}
+		return damage;
+	}
+
+	// Problem 19
+	public static int swapsToSave(String[] instr, int field) {
+		int swaps = 0;
+		
+		return swaps;
+	}
+	// End Problem 19
+
+	private static void swap(Message[] arr, int idx1, int idx2) {
+		Message temp = arr[idx1];
+		arr[idx1] = arr[idx2];
+		arr[idx2] = temp;
+	}
+
+	private static void sortMsgs(Message[] msgs) {
+		for (int i = 1; i < msgs.length; i++) {
+			int j = i;
+			while (j > 0 && msgs[j].compare(msgs[j - 1]) < 0) {
+				swap(msgs, j--, j);
+			}
+		}
+	}
+
+	// Problem 20
+	public static void decodeMessage(File file) throws FileNotFoundException {
+		Scanner scanner = new Scanner(file);
+
+		int lines = scanner.nextInt();
+		scanner.nextLine();
+
+		Message[] msgs = new Message[lines];
+
+		for (int i = 0; i < lines; i++) {
+			Message msg = new Message(
+				scanner.nextInt(), 
+				scanner.nextInt(), 
+				scanner.nextLine().trim()
+			);
+			msgs[i] = msg;
+		}
+
+		sortMsgs(msgs);
+
+		int prevNum = Integer.MIN_VALUE;
+		for (int i = 0; i < msgs.length; i++) {
+			if (msgs[i].getMessageNum() != prevNum) {
+				prevNum = msgs[i].getMessageNum();
+				System.out.println("Message " + msgs[i].getMessageNum() + ":");
+			}
+			System.out.println(msgs[i].getText());
+		}
+	}
 
 
 
