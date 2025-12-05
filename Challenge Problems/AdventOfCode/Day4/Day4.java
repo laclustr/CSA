978273b6cfd156c1669c7c9b2ae55445f4ce41ce
laclustr <28vance.grant@rrcs.org>
2025-12-05 13:12:00 -0500

    new aoc

---
+++ Challenge Problems/AdventOfCode/Day4/Day4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Day4 {
+	public static void main(String[] args) {
+		
+	}
+
+	public static long problem4() {
+		char[][] data;
+		ArrayList<ArrayList<Character>> 
+
+	}
+}
---
+++ Challenge Problems/AdventOfCode/Day5/Day5.java
@@ -0,0 +1,131 @@
+import java.util.*;
+import java.io.*;
+
+@SuppressWarnings("unchecked")
+public class Day5 {
+	static class Range {
+		long start;
+		long end;
+
+		public Range(long start, long end) {
+			this.start = start;
+			this.end = end;
+		}
+
+		public String toString() {
+			return "Range(" + start + ", " + end + ")";
+		}
+
+		public boolean inRange(long i) {
+			return (i >= start && i <= end);
+		}
+
+		public void set(long start, long end) {
+			this.start = start;
+			this.end = end;
+		}
+
+		public boolean overlaps(Range other) {
+			long overlapStart = Math.max(this.start, other.start);
+			long overlapEnd = Math.min(this.end, other.end);
+
+			if (overlapStart > overlapEnd) return true;
+			return false;
+		}
+
+		public Range getFullOverlap(Range other) {
+			if (this.overlap(other)) {
+				return new Range(Math.min(other.start, this.start), Math.max(other.end, this.end));
+			}
+			return null;
+		}
+
+		public long getDiff() {
+			return end - start + 1;
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(part1());
+		System.out.println(part2());
+	}
+
+	private static ArrayList<ArrayList> parseData() {
+		ArrayList<Range> ranges = new ArrayList<>();
+		ArrayList<Long> ids = new ArrayList<>();
+
+		boolean loading = true;
+
+		try (Scanner sc = new Scanner(new File("input.txt"))) {
+			while (sc.hasNext()) {
+				String line = sc.nextLine();
+
+				if (line.equals("")) {
+					loading = false;
+					continue;
+				}
+
+				if (loading) {
+					String[] parts = line.split("-");
+					long start = Long.parseLong(parts[0]);
+					long end = Long.parseLong(parts[1]);
+
+					ranges.add(new Range(start, end));
+				} else {
+					ids.add(Long.parseLong(line));
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		}
+
+		ArrayList<ArrayList> res = new ArrayList<>();
+		res.add(ranges);
+		res.add(ids);
+
+		return res;
+	}
+
+	public static long part1() {
+		ArrayList<ArrayList> parsed = parseData();
+
+		ArrayList<Range> ranges = parsed.get(0);
+		ArrayList<Long> ids = parsed.get(1);
+
+		long nFresh = 0;
+
+		for (long id : ids) {
+			for (Range range : ranges) {
+				if (range.inRange(id)) {
+					nFresh++;
+					break;
+				}
+			}
+		}
+		return nFresh;
+	}
+
+	public static long part2() {
+		ArrayList<ArrayList> parsed = parseData();
+
+		ArrayList<Range> ranges = parsed.get(0);
+
+		long k = ranges.length() - 1;
+
+		for (int i = 0; i < k; i++) {
+			if (ranges.get(i).overlaps(ranges.get(i + 1))) {
+				ranges.set(i, ranges.get(i).getFullOverlap(ranges.get(i + 1)));
+				ranges.remove(i + 1);
+				k--;
+				i--;
+				continue;
+			}
+		}
+
+		long total = 0;
+		for (Range range : ranges) {
+			total += range.getDiff();
+		}
+		return total;
+	}
+}
